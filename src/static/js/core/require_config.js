6e3bb0cf3f5989065874608988896a9a3744e7b3
rodickmini <rodickcai@gmail.com>
2016-03-23 12:13:43 +0800

    upd

--- src/static/js/core/require_config.js
+++
@@ -1,9 +0,0 @@
-requirejs.config({
-  baseUrl: "/static/js/",
-  paths: {
-    fastclick: 'lib/fastclick/lib/fastclick',
-    iscroll: 'lib/iscroll/build/iscroll',
-    nicelib: 'lib/nicelibrary/dist/js',
-    __has_map: ''
-  }
-});
